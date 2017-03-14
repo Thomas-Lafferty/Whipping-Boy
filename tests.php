8ec06e80b656fe9a2167af26598f6a99169c86e7
mikecroall <mikebcroall@gmail.com>
2017-03-14 15:48:38 +0000

    formatting

--- tests.php
+++ tests.php
@@ -2,8 +2,9 @@
 
 class StartTest extends PHPUnit_Framework_TestCase
 {
-  /** @test */
-  public function firstTest() {
-    $this->assertEquals(1+2,3);
-  }
- }
+    /** @test */
+    public function firstTest()
+    {
+        $this->assertEquals(1+2, 3);
+    }
+}
