c8337f78fd132999ae37edc25e102ea10058c5f2
mikecroall <mikebcroall@gmail.com>
2017-03-14 15:38:58 +0000

    COLOURS BETTER GOOD

--- index.php
+++ index.php
@@ -17,32 +17,42 @@
 			"Context-Free",
 			"Plagiarism"
 		];
-		var colours = [
-			"#f00", "#0f0", "#00f",
-			"#f0f", "#0ff", "#ff0",
-			"#faf", "#aff", "#ffa"
-		];
+
+		function getRandomColour() {
+			var r = Math.floor(Math.random() * 255),
+			g = Math.floor(Math.random() * 255),
+			b = Math.floor(Math.random() * 255);
+			return "rgba(" + r + "," + g + "," + b + ",1.0)";
+		}
 
 		function update() {
 			width += (Math.random() * 2 * max_change) - max_change;
 			if (width > max_width) { width = max_width; }
 			if (width < min_width) { width = min_width; }
 			document.getElementById("myimg").width = width;
+
 			if (Math.random() < 0.1) {
 				document.getElementById("myh").innerHTML = messages[Math.floor(Math.random() * messages.length)];
 			}
+			// NOTE below should remain a separate event than above, despite equal probability
+			if (Math.random() < 0.1) {
+				document.getElementById("mybody").style.backgroundColor = getRandomColour();
+			}
 			if (Math.random() < 0.15) {
 				left = !left;
+				document.getElementById("myimg").style.float = left ? "left" : "right";
 			}
-			document.getElementById("myh").style.color = colours[Math.floor(Math.random() * colours.length)];
-			document.getElementById("myimg").style.float = left ? "left" : "right";
+
+			document.getElementById("myh").style.color = getRandomColour();
+
 			window.requestAnimationFrame(update);
 		}
+		
 		window.requestAnimationFrame(update);
 	</script>
 </head>
 
-<body>
+<body id="mybody">
   <form method="post" action="maps.php">
 	  Location:
 	  <input type="text" name="location">
